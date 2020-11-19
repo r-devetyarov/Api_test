c7e53e08dc7f9551995b74839dc4f49a9923cd9e
renat <renat.devetyarov@bk.ru>
2020-11-19 17:56:08 +0300

    Edit fixture , adding class with constructor in conftest.py

--- lesson_2_pytest_fixture_and_other/conftest.py
+++ lesson_2_pytest_fixture_and_other/conftest.py
@@ -1,13 +1,6 @@
 import pytest
 
 
-@pytest.fixture(scope='function')
-def create_list_7():
-    new_list = [x for x in range(9999) if x % 7 == 0]
-    print('\n Use function fixture')
-    return new_list
-
-
 @pytest.fixture(scope='module', params=[4, 8, 12, 16])
 def create_first_number(request):
     print('\n Use module fixture')
@@ -25,3 +18,22 @@ def create_dict(create_list_7, create_sum):
     new_dict = {'key_1': create_sum, 'key_2': create_list_7, 'key3': 'value'}
     print('\n User function fixture')
     return new_dict
+
+
+class FixtureClass:
+
+    def __init__(self, length, step):
+        self.length = length
+        self.step = step
+
+    def value_returner(self):
+        return self.length, self.step
+
+
+@pytest.fixture(scope='function')
+def create_list_7():
+    const = FixtureClass(length=999, step=7)
+    length, step = const.value_returner()
+    new_list = [x for x in range(length) if x % step == 0]
+    print('\n Use function fixture')
+    return new_list
