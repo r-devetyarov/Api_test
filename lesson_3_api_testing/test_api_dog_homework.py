ce14aeb5ae84d00affee176246a142f229d288da
renat <renat.devetyarov@bk.ru>
2020-11-22 22:29:59 +0300

    Add test for dog api, mini change requirements.txt

--- lesson_3_api_testing/test_api_dog_homework.py
+++ lesson_3_api_testing/test_api_dog_homework.py
@@ -10,3 +10,21 @@ class TestApiDog:
         assert response.status_code == 200
         assert 'message' in response.json()
         assert response.json()['status'] == 'success'
+
+    def test_get_random_image(self, dog_api_client, path='breeds/image/random'):
+        response = dog_api_client.get_request(path=path)
+        assert response.status_code == 200
+        assert 'message' in response.json()
+        assert 'status' in response.json()
+        assert response.json()['status'] == 'success'
+
+    @pytest.mark.parametrize('valid_count', [x for x in range(1, 50, 1)])
+    def test_get_random_image_count(self, dog_api_client, valid_count):
+        path = f'breeds/image/random/{valid_count}'
+        response = dog_api_client.get_request(path=path)
+        assert response.status_code == 200
+        assert 'message' in response.json()
+        assert 'status' in response.json()
+        assert response.json()['status'] == 'success'
+        assert len(response.json()['message']) == valid_count
+
