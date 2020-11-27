855f56e0c8fbb06355575c6936dcc07bb28426f4
renat <renat.devetyarov@bk.ru>
2020-11-27 13:36:11 +0300

    Final version homework_ddt.py

--- lesson_4_DDT/homework_ddt.py
+++ lesson_4_DDT/homework_ddt.py
@@ -1,14 +1,12 @@
 file = open('data.csv')
-# for line in file:
-#     print(line)
 data_list = file.readlines()
 data_list.pop(0)
-# print(data_list[1])
-# split() - разбивает строку в список по пробелам
+
 data_name = []
 data_lastname = []
 data_middle_name = []
 data_city = []
+
 for item in data_list:
     r = item.split()
     data_lastname.append(r[0])
@@ -25,13 +23,15 @@ new_data_list = [[lastname, name, middle_name, city, credit_card, deposit, mortg
                  for deposit in ['+', '-']
                  for mortgage in ['+', '-']]
 
-# print(new_data_list)
-
 
 def data_test_create():
-    data_file = open('test123.txt', 'r')
-    print(data_file)
-    # data_file.write('asdasd')
-        # for i in range(10):
-        #     print(new_data_list[i][0], file=data_file)
+    data_file = open('test.txt', 'a')
+    for i in new_data_list:
+        for word in i:
+            data_file.write(word)
+            data_file.write(' ')
+        data_file.write('\n')
+    data_file.close()
 
+
+data_test_create()
